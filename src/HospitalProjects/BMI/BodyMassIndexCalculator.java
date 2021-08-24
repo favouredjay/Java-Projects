7005a1c83ad0efe889ad268b2f122ceed923d0db
favouredjay <judom.2011@gmail.com>
2021-08-24 15:35:16 +0100

    projects

--- src/HospitalProjects/BMI/BodyMassIndexCalculator.java
+++ src/HospitalProjects/BMI/BodyMassIndexCalculator.java
@@ -1,6 +1,8 @@
 package HospitalProjects.BMI;
 
 
+import HospitalProjects.BMI.Exception.InvalidInputException;
+
 import java.util.Scanner;
 
 public class BodyMassIndexCalculator {
@@ -11,49 +13,60 @@ public class BodyMassIndexCalculator {
     private double weight;
     Scanner input = new Scanner(System.in);
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
         System.out.println("Hello, please Enter your name");
         this.name = input.next();
-        System.out.println("Hello" +" " + this.name);
+        System.out.println("Hello" + " " + this.name);
     }
 
     public String getName() {
         return name;
     }
 
-    public void setAge(int age){
-
-        System.out.println("How old are you?");
-        age = input.nextInt();
+    public void setAge(int age) throws InvalidInputException {
+        boolean validateAge;
+//        System.out.println("How old are you?");
+//        age = input.nextInt();
+        validateAge = age > 0 && age <= 200;
+        if (!validateAge) {
+            throw new InvalidInputException();
+        }
         this.age = age;
     }
+    public int getAge(){
+        return age;
+    }
 
-    public void setGender(String gender){
+    public void setGender(String gender) {
         System.out.println("Enter your gender");
         gender = input.next();
         this.gender = gender;
     }
+
     public double getHeight() {
         return height;
     }
-    public double getWeight(){
+
+    public double getWeight() {
         return weight;
     }
 
 
-    public void setFullInfo(){
+    public void setFullInfo() throws InvalidInputException {
         setName(name);
-        setAge(age);
+//        setAge(age);
         setGender(gender);
-       calculateHeight();
-       calculateWeight();
+        calculateHeight();
+        calculateWeight();
 
     }
-    public String toString(){
+
+    public String toString() {
         System.out.println();
-        return (name + "\n"+ "Your BMI is " + this.bmi());
+        return (name + "\n" + "Your BMI is " + this.bmi());
     }
+
     public double bmi() {
 
         double bmi = getWeight() / (Math.pow(getHeight(), 2));
@@ -67,7 +80,7 @@ public class BodyMassIndexCalculator {
         }
         if (bmi >= 25 && bmi < 30) {
             System.out.println("Underweight");
-        } else if (bmi >= 30){
+        } else if (bmi >= 30) {
             System.out.println("Obese");
         }
         System.out.println("BMI VALUES");
@@ -75,9 +88,11 @@ public class BodyMassIndexCalculator {
         System.out.println("Normal: \tbetween 18.5 and 24.9");
         System.out.println("Overweight: \tbetween 25 and 29.9");
         System.out.println("Obese: \t\t30 or greater");
-return bmi;
+        return bmi;
     }
-    public void calculateHeight(){
+
+    public void calculateHeight() {
+
         System.out.println("""
                 Enter Height in meters or foot
                 Press 1 to enter height in meters
@@ -86,8 +101,8 @@ return bmi;
 
         int heightInput = input.nextInt();
 
-        switch (heightInput){
-            case 1-> {
+        switch (heightInput) {
+            case 1 -> {
 
                 System.out.println("Enter height in meters");
                 height = input.nextDouble();
@@ -95,14 +110,17 @@ return bmi;
                 System.out.println(height);
             }
 
-            case 2-> {
+            case 2 -> {
                 System.out.println("Enter height in foot");
                 height = input.nextDouble();
 //                setHeight(height);
                 height = height * (0.305);
                 System.out.println(height);
             }
-            default-> System.out.println();
+            default -> {
+                System.out.println("Invalid Selection");
+                calculateHeight();
+            }
 
         }
     }
@@ -110,10 +128,9 @@ return bmi;
     public void calculateWeight() {
         System.out.println("""
                 Enter Weight in kg or pounds
-                Press 1 to enter weight in pound
-                Press 2 to enter weight in kg
+                Press 1 to enter weight in kg
+                Press 2 to enter weight in pounds
                 """);
-
         int weightInput = input.nextInt();
 
         switch (weightInput) {
@@ -127,44 +144,56 @@ return bmi;
             case 2 -> {
                 System.out.println("Enter weight in pounds");
                 weight = input.nextDouble();
-                weight = weight * (2.20462);
+                weight = weight * (0.453592);
                 System.out.println(weight);
             }
-            default -> System.out.println();
+            default -> {
+                System.out.println("Invalid selection");
+                calculateWeight();
+            }
+
 
         }
     }
-    public static void main(String[] args) {
-        BodyMassIndexCalculator bmiCalculator = new BodyMassIndexCalculator();
-        Scanner userInput = new Scanner(System.in);
-        int input;
-        boolean quit = false;
-        System.out.println("""
-                Welcome to Jay's Bmi calculator
-                Best Wishes
-                                
-                please press 1 to continue or 2 to cancel
-                """);
-        input = userInput.nextInt();
-        while (!quit) {
-            switch (input) {
-                case 1 -> {
-                    bmiCalculator.setFullInfo();
-                    System.out.println();
-                    System.out.println(bmiCalculator);
-                    System.out.println(bmiCalculator.bmi());
 
-break;
+
+    public static void main(String[] args) throws InvalidInputException {
+        try {
+            BodyMassIndexCalculator bmiCalculator = new BodyMassIndexCalculator();
+            boolean quit = false;
+            while (!quit) {
+                Scanner userInput = new Scanner(System.in);
+                int input;
+
+                System.out.println("""
+                        Welcome to Jay's Bmi calculator
+                        Best Wishes
+                                        
+                        please press 1 to continue or 2 to cancel
+                        """);
+                input = userInput.nextInt();
+
+                switch (input) {
+
+                    case 1 -> {
+                        bmiCalculator.setFullInfo();
+                        System.out.println();
+                        System.out.println(bmiCalculator.bmi());
+                        System.out.println(bmiCalculator);
+
+
+                    }
+                    default -> quit = true;
+
+
                 }
-                case 2 -> quit = true;
-
-
             }
-        }
 
+        } catch (InvalidInputException e) {
+            e.printStackTrace();
+        }
     }
 }
-
 
 
 
--- test/HospitalProjects/BMI/BodyMassIndexCalculatorTest.java
+++ test/HospitalProjects/BMI/BodyMassIndexCalculatorTest.java
@@ -1,7 +1,26 @@
 package HospitalProjects.BMI;
 
+import HospitalProjects.BMI.Exception.InvalidInputException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BodyMassIndexCalculatorTest {
+    BodyMassIndexCalculator calculator = new BodyMassIndexCalculator();
+    @BeforeEach
+    void setUp() throws InvalidInputException {
+        calculator.setName("Sarah");
+        calculator.setGender("female");
+        calculator.setAge(221);
+    }
+    @Test
+    void testThatAgeIsInvalid(){
+        assertThrows(InvalidInputException.class,()->calculator.getAge());
+    }
 
+//    @Test
+//    void testThatPatientIsNotNull(){
+//        assertNotNull(calculator);
+//    }
 }
