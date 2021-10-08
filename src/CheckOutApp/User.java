03bf49c2685051c44d70290ab4abae4b6164f5b2
favouredjay <judom.2011@gmail.com>
2021-10-08 13:31:29 +0100

    checkoutapp

--- src/CheckOutApp/User.java
+++ src/CheckOutApp/User.java
@@ -1,2 +1,22 @@
-package CheckOutApp;public class User {
+package CheckOutApp;
+
+public class User {
+    private String nameOfCustomer;
+    private String nameOfCashier;
+
+    public String getNameOfCustomer() {
+        return nameOfCustomer;
+    }
+
+    public void setNameOfCustomer(String nameOfCustomer) {
+        this.nameOfCustomer = nameOfCustomer;
+    }
+
+    public String getNameOfCashier() {
+        return nameOfCashier;
+    }
+
+    public void setNameOfCashier(String nameOfCashier) {
+        this.nameOfCashier = nameOfCashier;
+    }
 }
